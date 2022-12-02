82e1afb431ddbfe6eaef3c74a22003fe1dd4f1a6
U-DESKTOP-7696U09\Alex <314159265358x@gmail.com>
2022-12-02 16:06:34 +0800

    .

--- src/main/java/biomesoplenty/common/fluids/blocks/BlockHotSpringWaterFluid.java
+++ src/main/java/biomesoplenty/common/fluids/blocks/BlockHotSpringWaterFluid.java
@@ -10,7 +10,6 @@ package biomesoplenty.common.fluids.blocks;
 
 import java.util.Random;
 
-import biomesoplenty.api.block.BOPBlocks;
 import net.minecraft.block.Block;
 import net.minecraft.block.material.MapColor;
 import net.minecraft.block.material.Material;
@@ -19,8 +18,6 @@ import net.minecraft.block.state.IBlockState;
 import net.minecraft.entity.Entity;
 import net.minecraft.entity.EntityLivingBase;
 import net.minecraft.init.Blocks;
-import net.minecraft.init.MobEffects;
-import net.minecraft.potion.PotionEffect;
 import net.minecraft.util.EnumFacing;
 import net.minecraft.util.EnumParticleTypes;
 import net.minecraft.util.math.AxisAlignedBB;
@@ -32,106 +29,94 @@ import net.minecraftforge.fluids.Fluid;
 import net.minecraftforge.fml.relauncher.Side;
 import net.minecraftforge.fml.relauncher.SideOnly;
 
-public class BlockHotSpringWaterFluid extends BlockFluidClassic
-{
-    public BlockHotSpringWaterFluid(Fluid fluid)
-    {
-        super(fluid, Material.WATER);
-        this.setLightOpacity(3);
-        this.setHardness(100.0F);
-    }
+public class BlockHotSpringWaterFluid extends BlockFluidClassic {
+	public BlockHotSpringWaterFluid(Fluid fluid) {
+		super(fluid, Material.WATER);
+		this.setLightOpacity(3);
+		this.setHardness(100.0F);
+	}
 
-    @Override
-    public void onEntityCollidedWithBlock(World world, BlockPos pos, IBlockState state, Entity entity)
-    {
-        if (entity instanceof EntityLivingBase)
-        {
-            ((EntityLivingBase)entity).addPotionEffect(new PotionEffect(MobEffects.REGENERATION, 100));
-        }
-    }
-    
-    @Override
-    @SideOnly(Side.CLIENT)
-    public void randomDisplayTick(IBlockState state, World world, BlockPos pos, Random rand) 
-    {
-        super.randomDisplayTick(state, world, pos, rand);
-        if (rand.nextInt(25)==0)
-        {           
-        	world.spawnParticle(EnumParticleTypes.CLOUD, pos.getX() + rand.nextFloat(), pos.getY() + 1.0F, pos.getZ() + rand.nextFloat(), 0.0D, 0.0D, 0.0D);
-        }
-    }
+	@Override
+	public void onEntityCollidedWithBlock(World world, BlockPos pos, IBlockState state, Entity entity) {
+		if (!world.isRemote) {
+			if (entity instanceof EntityLivingBase) {
+				EntityLivingBase entityLiving = (EntityLivingBase) entity;
+				if (entityLiving.ticksExisted % 20 == 0) {
+					entityLiving.heal(0.5f);
+				}
+			}
+		}
+	}
 
-    @Override
-    public AxisAlignedBB getBoundingBox(IBlockState state, IBlockAccess source, BlockPos pos)
-    {
-        return FULL_BLOCK_AABB;
-    }
+	@Override
+	@SideOnly(Side.CLIENT)
+	public void randomDisplayTick(IBlockState state, World world, BlockPos pos, Random rand) {
+		super.randomDisplayTick(state, world, pos, rand);
+		if (rand.nextInt(25) == 0) {
+			world.spawnParticle(EnumParticleTypes.CLOUD, pos.getX() + rand.nextFloat(), pos.getY() + 1.0F,
+					pos.getZ() + rand.nextFloat(), 0.0D, 0.0D, 0.0D);
+		}
+	}
 
-    @Override
-    public AxisAlignedBB getCollisionBoundingBox(IBlockState blockState, IBlockAccess worldIn, BlockPos pos)
-    {
-        return NULL_AABB;
-    }
-    
-    @Override
-    public void onBlockAdded(World worldIn, BlockPos pos, IBlockState state)
-    {
-    	super.onBlockAdded(worldIn, pos, state);
-        this.checkForMixing(worldIn, pos, state);
-    }
+	@Override
+	public AxisAlignedBB getBoundingBox(IBlockState state, IBlockAccess source, BlockPos pos) {
+		return FULL_BLOCK_AABB;
+	}
 
-    @Override
-    public void neighborChanged(IBlockState state, World worldIn, BlockPos pos, Block blockIn, BlockPos neighborPos)
-    {
-    	super.neighborChanged(state, worldIn, pos, blockIn, neighborPos);
-        this.checkForMixing(worldIn, pos, state);
-    }
-    
-    public boolean checkForMixing(World worldIn, BlockPos pos, IBlockState state)
-    {
-        boolean flag = false;
+	@Override
+	public AxisAlignedBB getCollisionBoundingBox(IBlockState blockState, IBlockAccess worldIn, BlockPos pos) {
+		return NULL_AABB;
+	}
 
-        for (EnumFacing enumfacing : EnumFacing.values())
-        {
-            if (enumfacing != EnumFacing.DOWN && (worldIn.getBlockState(pos.offset(enumfacing)).getMaterial().isLiquid() == true))
-            {
-            	if (worldIn.getBlockState(pos.offset(enumfacing)).getBlock() != this.getBlockState().getBlock())
-            	{
-	                flag = true;
-	                break;
-            	}
-            }
-        }
+	@Override
+	public void onBlockAdded(World worldIn, BlockPos pos, IBlockState state) {
+		super.onBlockAdded(worldIn, pos, state);
+		this.checkForMixing(worldIn, pos, state);
+	}
 
-        if (flag)
-        {
-            Integer integer = state.getValue(LEVEL);
+	@Override
+	public void neighborChanged(IBlockState state, World worldIn, BlockPos pos, Block blockIn, BlockPos neighborPos) {
+		super.neighborChanged(state, worldIn, pos, blockIn, neighborPos);
+		this.checkForMixing(worldIn, pos, state);
+	}
 
-            if (integer.intValue() == 0)
-            {
-            	worldIn.setBlockState(pos, Blocks.STONE.getDefaultState());
-                return true;
-            }
+	public boolean checkForMixing(World worldIn, BlockPos pos, IBlockState state) {
+		boolean flag = false;
 
-            if (integer.intValue() <= 4)
-            {
-            	worldIn.setBlockState(pos, Blocks.STONE.getDefaultState());
-                return true;
-            }
-        }
+		for (EnumFacing enumfacing : EnumFacing.values()) {
+			if (enumfacing != EnumFacing.DOWN
+					&& (worldIn.getBlockState(pos.offset(enumfacing)).getMaterial().isLiquid() == true)) {
+				if (worldIn.getBlockState(pos.offset(enumfacing)).getBlock() != this.getBlockState().getBlock()) {
+					flag = true;
+					break;
+				}
+			}
+		}
 
-        return false;
-    }
-    
-    @Override
-    public MapColor getMapColor(IBlockState state, IBlockAccess world, BlockPos pos)
-    {
-        return MapColor.LIGHT_BLUE;
-    }
-    
-    @Override
-    public BlockFaceShape getBlockFaceShape(IBlockAccess world, IBlockState state, BlockPos pos, EnumFacing facing)
-    {
-        return BlockFaceShape.UNDEFINED;
-    }
+		if (flag) {
+			Integer integer = state.getValue(LEVEL);
+
+			if (integer.intValue() == 0) {
+				worldIn.setBlockState(pos, Blocks.STONE.getDefaultState());
+				return true;
+			}
+
+			if (integer.intValue() <= 4) {
+				worldIn.setBlockState(pos, Blocks.STONE.getDefaultState());
+				return true;
+			}
+		}
+
+		return false;
+	}
+
+	@Override
+	public MapColor getMapColor(IBlockState state, IBlockAccess world, BlockPos pos) {
+		return MapColor.LIGHT_BLUE;
+	}
+
+	@Override
+	public BlockFaceShape getBlockFaceShape(IBlockAccess world, IBlockState state, BlockPos pos, EnumFacing facing) {
+		return BlockFaceShape.UNDEFINED;
+	}
 }
